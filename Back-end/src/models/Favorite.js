5a96fcd29e5417e18409690241dc99676d3ba225
DJoacodalessa <joaquindalessa@gmail.com>
2023-10-21 17:47:32 -0300

    add more things about DB

--- Back-end/src/models/Favorite.js
+++ Back-end/src/models/Favorite.js
@@ -2,36 +2,49 @@ const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
    sequelize.define('Favorite', {
-      id:{
-         type: DataTypes.INTEGER,
-         allowNull: false,
+      id: {
+         type: DataTypes.STRING,
          primaryKey: true
       },
-      name:{
-         type: DataTypes.STRING, 
-         allowNull: false,
-         isEmail: true,
-   }, 
-   status: {
-      type:DataTypes.ENUM("Alive", "Dead", "unknown"),
-      allowNull: false,
-   },
-   species:{
-      type: DataTypes.STRING, 
-      allowNull: false,
-   },
-   gender:{
-      type:DataTypes.ENUM("Female", "Male", "Genderless", "unknown"),
-      allowNull: false,
-   },
-   origin:{
-      type: DataTypes.STRING, 
-      allowNull: false,
-   },
-   image:{
-      type: DataTypes.STRING, 
-      allowNull: false,
-   },
-},
-{ timestamps: false });
+      name: {
+         type: DataTypes.STRING,
+         unique: true,
+         allowNull: false
+      },
+      status: {
+         type: DataTypes.ENUM('Alive', 'Dead', 'unknown'),
+         defaultValue: 'Alive',
+         allowNull: false
+      },
+      species: {
+         type: DataTypes.ENUM(
+            'Human', 
+            'Humanoid', 
+            'Alien',
+            'Animal', 
+            'Robot', 
+            'Disease', 
+            'Parasite',
+            'Crononberg',
+            'Mythological Creature',
+            'Poopybutthole',
+            'unknown'),
+         defaultValue: 'unknown',
+         allowNull: false
+      },
+      gender: {
+         type: DataTypes.ENUM('Male', 'Female', 'Genderless', 'unknown'),
+         defaultValue: 'unknown',
+         allowNull: false
+      },
+      origin: {
+         type: DataTypes.STRING,
+         allowNull: false
+      },
+      image: {
+         type: DataTypes.STRING,
+         unique: true,
+         /* allowNull: false */
+      },
+   }, { timestamps: false });
 };
